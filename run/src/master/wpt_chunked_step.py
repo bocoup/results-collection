09e7ba963bc4db8381892b1ab1255a0332b5c7e5
Mike Pennisi <mike@mikepennisi.com>
2018-03-15 13:27:22 -0400

    Create dedicated "trigger" step for each browser
    This improves the structure as rendered in the Buildbot web UI, but more
    importantly, it enables future implementation of trigger-specific locks
    (i.e. for builds mediated by the Sauce Labs service).

--- run/src/master/wpt_chunked_step.py
+++ run/src/master/wpt_chunked_step.py
@@ -1,17 +1,15 @@
 from buildbot.plugins import steps
 
 class WPTChunkedStep(steps.Trigger):
-    def __init__(self, platform_manifest, total_chunks, *args, **kwargs):
-        super(WPTChunkedStep, self).__init__(*args, **kwargs)
-        self.platforms = []
-
-        for platform_id in platform_manifest:
-            platform_spec = platform_manifest[platform_id]
-
-            if platform_spec['currently_run']:
-                self.platforms.append(platform_spec)
-
+    def __init__(self, platform, total_chunks, *args, **kwargs):
+        self.platform = platform
         self.total_chunks = total_chunks
+
+        kwargs['name'] = str('Trigger %s chunks on %s' % (
+            total_chunks, platform['browser_name'].title()
+        ))
+
+        super(WPTChunkedStep, self).__init__(*args, **kwargs)
 
     def getSchedulersAndProperties(self):
         spec = []
@@ -19,19 +17,18 @@ class WPTChunkedStep(steps.Trigger):
         for scheduler in self.schedulerNames:
             unimportant = scheduler in self.unimportantSchedulerNames
 
-            for platform in self.platforms:
-                for this_chunk in range(1, self.total_chunks + 1):
-                    spec.append({
-                        'sched_name': scheduler,
-                        'props_to_set': {
-                            'this_chunk': this_chunk,
-                            'total_chunks': self.total_chunks,
-                            'browser_name': platform['browser_name'],
-                            'browser_version': platform['browser_version'],
-                            'os_name': platform['os_name'],
-                            'os_version': platform['os_version']
-                        },
-                        'unimportant': unimportant
-                    })
+            for this_chunk in range(1, self.total_chunks + 1):
+                spec.append({
+                    'sched_name': scheduler,
+                    'props_to_set': {
+                        'this_chunk': this_chunk,
+                        'total_chunks': self.total_chunks,
+                        'browser_name': self.platform['browser_name'],
+                        'browser_version': self.platform['browser_version'],
+                        'os_name': self.platform['os_name'],
+                        'os_version': self.platform['os_version']
+                    },
+                    'unimportant': unimportant
+                })
 
         return spec
