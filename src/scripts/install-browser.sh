52b336a5e516268f17bec3b42d96b722d7ec2e12
Mike Pennisi <mike@mikepennisi.com>
2018-04-09 20:57:12 -0400

    Add license information

--- src/scripts/install-browser.sh
+++ src/scripts/install-browser.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Copyright 2018 The WPT Dashboard Project. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
 browser_name=$1
 url=$2
 temp_file=$(mktemp)
