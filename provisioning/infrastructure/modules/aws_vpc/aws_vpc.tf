40ca636ef25ddb9ff687f29705544cc8576671a5
Mike Pennisi <mike@mikepennisi.com>
2018-06-01 20:58:50 -0400

    PARTIAL: Extending infrastructure with VPN
    When complete, this will allow a locally-managed device to serve as a
    worker in results collection.

--- provisioning/infrastructure/modules/aws_vpc/aws_vpc.tf
+++ provisioning/infrastructure/modules/aws_vpc/aws_vpc.tf
@@ -6,6 +6,7 @@ variable "cidr" { }
 
 output "id" { value = "${aws_vpc.main.id}" }
 output "cidr" { value = "${aws_vpc.main.cidr_block}" }
+output "vpn_gateway_id" { value = "${aws_vpn_gateway.main.id}" }
 
 resource "aws_vpc" "main" {
   cidr_block = "${var.cidr}"
@@ -14,4 +15,40 @@ resource "aws_vpc" "main" {
   tags {
     Name = "${var.name}"
   }
+}
+
+# Consider relocating the following into a module
+
+resource "aws_vpn_gateway" "main" {
+  vpc_id = "${aws_vpc.main.id}"
+  lifecycle {
+    create_before_destroy = true
+  }
+  tags {
+    Name = "${var.name}"
+  }
+}
+
+resource "aws_customer_gateway" "customer_gateway" {
+  bgp_asn    = 65000
+  # The public IP address of the external machine
+  ip_address = "135.84.57.36"
+  type       = "ipsec.1"
+  tags {
+    Name = "${var.name}"
+  }
+}
+
+resource "aws_vpn_connection" "main" {
+  vpn_gateway_id      = "${aws_vpn_gateway.main.id}"
+  customer_gateway_id = "${aws_customer_gateway.customer_gateway.id}"
+  type                = "ipsec.1"
+  static_routes_only  = true
+}
+
+output "tunnel_address" {
+  value = "${aws_vpn_connection.main.tunnel1_address}"
+}
+output "tunnel_psk" {
+  value = "${aws_vpn_connection.main.tunnel1_preshared_key}"
 }
--- provisioning/infrastructure/outputs.tf
+++ provisioning/infrastructure/outputs.tf
@@ -1,3 +1,11 @@
 output "master_public_ip" {
   value = "${aws_instance.build_master.public_ip}"
 }
+
+output "vpn_tunnel_address" {
+  value = "${module.vpc.tunnel_address}"
+}
+
+output "vpn_tunnel_psk" {
+  value = "${module.vpc.tunnel_psk}"
+}
