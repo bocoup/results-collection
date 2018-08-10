b23adbc83a1eaa04e962c7d323f3d3fa5220af2e
Mike Pennisi <mike@mikepennisi.com>
2018-08-09 20:00:42 -0400

    Complete installation script

--- src/scripts/install-webdriver.sh
+++ src/scripts/install-webdriver.sh
@@ -4,4 +4,54 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+browser_name=$1
+url=$2
+temp_file=$(mktemp)
 
+install_chromedriver() {
+  archive=$1
+  target=$(readlink --canonicalize .)/chromedriver
+
+  rm --force $target
+
+  unzip $archive || return 1
+
+  chown --recursive $SUDO_USER $target >&2 || return 1
+
+  echo $target
+}
+
+install_geckodriver() {
+  archive=$1
+  target=$(readlink --canonicalize .)/geckodriver
+
+  rm --force $target
+
+  tar -xvf $archive >&2 || return 1
+
+  chown --recursive $SUDO_USER $target >&2 || return 1
+
+  echo $target
+}
+
+wget --quiet --output-document $temp_file $url
+
+if [ $? != '0' ]; then
+  echo Error downloading browser. >&2
+  exit 1
+fi
+
+if [ $browser_name == 'chrome' ]; then
+  install_chromedriver $temp_file
+elif [ $browser_name == 'firefox' ]; then
+  install_geckodriver $temp_file
+else
+  echo Unrecognized browser: $browser_name >&2
+  false
+fi
+
+result=$?
+
+rm --force $temp_file
+
+exit $result
